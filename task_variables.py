b8b813c340b7f5e28954d9618f1f35f1bdc6c14d
Anusha B <anusha2676@gmail.com>
2026-04-05 11:40:42 +0530

    Merged all variable tasks into one file

---
+++ task_variables.py
@@ -0,0 +1,28 @@
+# ===============================
+# VARIABLES TASK
+# ===============================
+
+# Task 1: Create pi and check its data type
+pi = 22/7
+print("Value of pi:", pi)
+print("Data type of pi:", type(pi))
+
+
+# -------------------------------
+
+# Task 2: Using reserved keyword as variable
+
+# for = 4   # This will cause SyntaxError
+print("Cannot use 'for' as variable name because it is a keyword in Python")
+
+
+# -------------------------------
+
+# Task 3: Simple Interest Calculation
+
+P = 1000
+R = 5
+T = 3
+
+SI = (P * R * T) / 100
+print("Simple Interest:", SI) 
